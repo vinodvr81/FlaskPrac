7248b5c92d73f9f5f2ed9f228b71d76a7421ae99
vinodvr81 <vinod.vukkalam@gmail.com>
2019-03-25 18:15:29 +0530

    added redirectExample

---
+++ thirteenRedirectFuncEx/redirectImplemen.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect,url_for,render_template,request
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('login.html')
+
+@app.route("/login",methods=['POST','GET'])
+def login():
+    if request.method == 'POST' and request.form['username'] == 'admin':
+        return redirect(url_for('success'))
+    return redirect(url_for('index'))
+
+@app.route("/success")
+def success():
+    return "logged in successfully"
+
+if __name__=='__main__':
+    app.run(debug=True)
+
